a7dc6ac691783deea21718b50bb1a627b1f5d028
Ulya Panwasusana <241226004@mhs.stiki.ac.id>
2026-08-02 01:09:53 +0700

    Add custom authorization middleware

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -2,11 +2,8 @@
 
 namespace App\Models;
 
-use App\Models\Permission;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-
-
 
 class Permission extends Model
 {
