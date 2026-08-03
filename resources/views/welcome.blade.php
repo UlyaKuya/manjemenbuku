17e555cb412dda25d12509d5181de72357a72357
Ulya Panwasusana <241226004@mhs.stiki.ac.id>
2026-08-03 12:52:06 +0700

    Deploy to Render

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>KendedesGo - UAS Proyek Akhir</title>
+    <title>Perpustakaan - UAS Proyek Akhir</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
